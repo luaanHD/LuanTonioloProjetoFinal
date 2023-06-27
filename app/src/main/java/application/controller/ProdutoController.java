b74cd50552d60716989e036b7ef9d635a40502b1
luaanHD <luantoniolo94@gmail.com>
2023-06-27 14:47:07 +0000

    Projeto Finalizado

--- app/src/main/java/application/controller/ProdutoController.java
+++ app/src/main/java/application/controller/ProdutoController.java
@@ -1,5 +1,7 @@
 package application.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,20 +19,20 @@ public class ProdutoController {
     private ProdutoRepository produtoRepo;
 
     @RequestMapping("/list")
-    public String list(Model model){
+    public String list(Model model) {
         model.addAttribute("produtos", produtoRepo.findAll());
         return "/produto/list";
     }
 
     @RequestMapping("/insert")
-    public String insert(){
+    public String insert() {
         return "/produto/insert";
     }
 
     @RequestMapping(value = "/insert", method = RequestMethod.POST)
     public String insert(
-        @RequestParam("titulo") String titulo,
-        @RequestParam("descricao") String descricao) {
+            @RequestParam("titulo") String titulo,
+            @RequestParam("descricao") String descricao) {
 
         Produto produto = new Produto();
         produto.setTitulo(titulo);
@@ -38,5 +40,70 @@ public class ProdutoController {
 
         produtoRepo.save(produto);
         return "redirect:/produto/list";
+    }
+
+    @RequestMapping("/update")
+    public String update(Model model, @RequestParam("id") int id) {
+        Optional<Produto> produto = produtoRepo.findById(id);
+
+        if (produto.isPresent()) {
+            model.addAttribute("produto", produto.get());
+            return "/produto/update";
         }
+
+        return "redirect:/produto/list";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+
+    public String update(
+
+            @RequestParam("id") int id,
+            @RequestParam("titulo") String titulo,
+            @RequestParam("descricao") String descricao) {
+
+        Optional<Produto> produto = produtoRepo.findById(id);
+
+        if (produto.isPresent()) {
+
+            produto.get().setTitulo(titulo);
+
+            produto.get().setDescricao(descricao);
+
+            produtoRepo.save(produto.get());
+
+        }
+
+        return "redirect:/produto/list";
+
+    }
+
+    @RequestMapping("/delete")
+
+    public String delete(Model model, @RequestParam("id") int id) {
+
+        Optional<Produto> produto = produtoRepo.findById(id);
+
+        if (produto.isPresent()) {
+
+            model.addAttribute("produto", produto.get());
+
+            return "/produto/delete";
+
+        }
+
+        return "redirect:/produto/list";
+
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+
+    public String delete(@RequestParam("id") int id) {
+
+        produtoRepo.deleteById(id);
+
+        return "redirect:/produto/list";
+
+    }
+
 }
